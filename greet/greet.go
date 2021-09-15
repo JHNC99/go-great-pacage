b3339fa0e259209e2ad7a5338fcdf8d42913580c
JHNC99 <jonathanhnc75@gmail.com>
2021-09-15 01:59:12 -0500

    first commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "✋"
+
+//English retorna saludos en ingles
+func Eglish() string {
+	return "Hi" + emoji
+}
+
+//Italian retorna saludo en italiano
+func Italian() string {
+	return "Ciao" + emoji
+}
